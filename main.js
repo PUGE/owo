167c217e2e0d9bb431101bfd4460144db9b844d2
PUGE <100284685@qq.com>
2019-01-06 15:45:40 +0800

    初步实现路由功能

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ const headPath = path + 'head/'
 const bodyPath = path + 'body/'
 const outPutPath = path + 'dist/'
 
+const config = {
+  entry: 'hellow'
+}
+
 // 读取模板文件
 // 将整个文件一次性读取到内存中
 let templet = fs.readFileSync(`${path}index.html`, 'utf8')
@@ -15,10 +19,14 @@ let templet = fs.readFileSync(`${path}index.html`, 'utf8')
 templet = heardHandle(headPath, templet)
 
 const dom = bodyHandle(bodyPath, templet)
-// console.log(dom)
-templet = dom.html
+
+// 读取出核心代码
+const configData = `\r\nvar globalConfig = ${JSON.stringify(config)} \r\n`
+const coreData = configData + fs.readFileSync(`./lib/core.js`, 'utf8')
+
 
 // console.log(templet)
 // 输出文件
-fs.writeFileSync(`${outPutPath}index.html`, templet)
+fs.writeFileSync(`${outPutPath}index.html`, dom.html)
 fs.writeFileSync(`${outPutPath}main.css`, dom.style)
+fs.writeFileSync(`${outPutPath}main.js`, coreData + dom.script)
