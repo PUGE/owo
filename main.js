4ae7ebbfc2a0e457a31cfad62b7ad65b2626e6c3
PUGE <mail@puge.me>
2019-01-10 14:29:30 +0800

    初步实现发布

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+#!/usr/bin/env node
+
 'use strict'
+
 const fs = require('fs')
 // 文件变动检测
 const chokidar = require('chokidar')
@@ -10,34 +13,21 @@ const UglifyJS = require("uglify-js")
 const heardHandle = require('./lib/heard')
 const bodyHandle = require('./lib/page')
 
-const path = './src/'
-const headPath = path + 'head/'
-const outPutPath = path + 'dist/'
-const corePath = './core/'
+const path = process.cwd()
 
-const config = {
-  entry: 'home'
-}
+// 读取配置文件
+const config = JSON.parse(fs.readFileSync(`${path}/ozzx.json`, 'utf8'))
 
-const packConfig = {
-  // 文件改变自动重新打包
-  autoPack: true,
-  // 压缩css
-  minifyCss: true,
-  // 是否压缩js
-  minifyJs: true,
-  // 页面存放目录
-  pagePath: path + 'page/',
-
-}
+const outPutPath = `${path}/${config.outFolder}/`
+const corePath = `${__dirname}/core/`
 
 function pack() {
   // 读取入口模板文件(一次性读取到内存中)
-  let templet = fs.readFileSync(`${path}index.html`, 'utf8')
+  let templet = fs.readFileSync(`${path}/index.html`, 'utf8')
   // 使用heard处理文件
-  templet = heardHandle(headPath, templet)
+  templet = heardHandle(`${path}/${config.headFolder}/`, templet)
 
-  const dom = bodyHandle(templet, packConfig)
+  const dom = bodyHandle(templet, config)
 
   // 读取出核心代码
   const configData = `
@@ -54,17 +44,17 @@ function pack() {
   fs.writeFileSync(`${outPutPath}index.html`, dom.html)
 
   // 读取出全局样式
-  const coreStyle = fs.readFileSync(`${corePath}main.css`, 'utf8') + '\r\n'
+  const coreStyle = fs.readFileSync(`${path}/main.css`, 'utf8') + '\r\n'
 
   // 判断是否需要压缩css
   let outPutCss = coreStyle + dom.style
-  if (packConfig.minifyCss) {
+  if (config.minifyCss) {
     outPutCss = minifier.minify(outPutCss)
   }
 
   // 判断是否需要压缩js
   let outPutJs = coreData + dom.script
-  if (packConfig.minifyJs) {
+  if (config.minifyJs) {
     outPutJs = UglifyJS.minify(outPutJs).code
   }
   fs.writeFileSync(`${outPutPath}main.css`, outPutCss)
@@ -78,8 +68,9 @@ pack()
 // 判断是否开启文件变动自动重新打包
 if (config) {
   // 文件变动检测
+  console.log('./' + config.outFolder + '/*')
   const watcher = chokidar.watch(path, {
-    ignored: outPutPath + '*',
+    ignored: './' + config.outFolder + '/*',
     persistent: true,
     usePolling: true
   })
