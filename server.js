172c00de2e88a6542c03ed5b1dba33912fefb5b1
PUGE <100284685@qq.com>
2019-03-12 23:07:14 +0800

    去掉部分样式

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ http.createServer(function (req, res) {
 
   //在end事件触发后，通过querystring.parse将post解析为真正的POST请求格式，然后向客户端返回。
   req.on('end', function () {
-    console.log(body)
     // 处理body
     const dom = bodyHandle(body, {
       "root": "/src",
