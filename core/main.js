32217b812335e28f7852b1dd5fb14ad65bfa875d
PUGE <100284685@qq.com>
2019-05-16 00:21:02 +0800

    修复ie下的兼容问题

--- core/main.js
+++ core/main.js
@@ -29,6 +29,25 @@ _owo.assign = function(a, b) {
   return newObj
 }
 
+// 针对低版本IE浏览器做兼容处理
+if (!document.getElementsByClassName) {
+  document.getElementsByClassName = function (className, element) {
+    var children = (element || document).getElementsByTagName('*');
+    var elements = new Array();
+    for (var i = 0; i < children.length; i++) {
+      var child = children[i];
+      var classNames = child.className.split(' ');
+      for (var j = 0; j < classNames.length; j++) {
+        if (classNames[j] == className) {
+          elements.push(child);
+          break;
+        }
+      }
+    }
+    return elements;
+  };
+}
+
 // 运行页面所属的方法
 _owo.handlePage = function (pageName, entryDom) {
   _owo.handleEvent(entryDom)
@@ -52,7 +71,7 @@ _owo.handlePage = function (pageName, entryDom) {
     if (templateScript.created) {
       // 获取到当前配置页的DOM
       // 待修复,临时获取方式,这种方式获取到的dom不准确
-      var domList = entryDom.getElementsByClassName('o-' + key)
+      var domList = document.getElementsByClassName('o-' + key)
       if (domList.length !== 1) {
         console.error('我就说会有问题吧!')
         console.log(domList)
@@ -71,86 +90,90 @@ _owo.handlePage = function (pageName, entryDom) {
 _owo.handleEvent = function (tempDom, templateName) {
   // console.log(templateName)
   var activePage = window.owo.script[owo.activePage]
-  for (let ind = 0; ind < tempDom.attributes.length; ind++) {
-    var attribute = tempDom.attributes[ind]
-    // 判断是否为owo的事件
-    // ie不支持startsWith
-    if (attribute.name[0] == '@') {
-      var eventName = attribute.name.slice(1)
-      var eventFor = attribute.textContent
-      switch (eventName) {
-        case 'show' : {
-          // 初步先简单处理吧
-          var temp = eventFor.replace(/ /g, '')
-          // 取出条件
-          const condition = temp.split("==")
-          if (activePage.data[condition[0]] != condition[1]) {
-            tempDom.style.display = 'none'
+  
+  if (tempDom.attributes) {
+    for (let ind = 0; ind < tempDom.attributes.length; ind++) {
+      var attribute = tempDom.attributes[ind]
+      // 判断是否为owo的事件
+      // ie不支持startsWith
+      if (attribute.name[0] == '@') {
+        var eventName = attribute.name.slice(1)
+        var eventFor = attribute.textContent
+        switch (eventName) {
+          case 'show' : {
+            // 初步先简单处理吧
+            var temp = eventFor.replace(/ /g, '')
+            // 取出条件
+            const condition = temp.split("==")
+            if (activePage.data[condition[0]] != condition[1]) {
+              tempDom.style.display = 'none'
+            }
+            break
           }
-          break
-        }
-        default: {
-          tempDom["on" + eventName] = function(event) {
-            // 因为后面会对eventFor进行修改所以使用拷贝的
-            var eventForCopy = eventFor
-            // 判断页面是否有自己的方法
-            var newPageFunction = window.owo.script[window.owo.activePage]
-            // console.log(this.attributes)
-            if (templateName) {
-              // 如果模板注册到newPageFunction中，那么证明模板没有script那么直接使用eval执行
-              if (newPageFunction.template) {
-                newPageFunction = newPageFunction.template[templateName]
-              } else {
-                eval(eventForCopy)
-                return
+          default: {
+            tempDom["on" + eventName] = function(event) {
+              // 因为后面会对eventFor进行修改所以使用拷贝的
+              var eventForCopy = eventFor
+              // 判断页面是否有自己的方法
+              var newPageFunction = window.owo.script[window.owo.activePage]
+              // console.log(this.attributes)
+              if (templateName) {
+                // 如果模板注册到newPageFunction中，那么证明模板没有script那么直接使用eval执行
+                if (newPageFunction.template) {
+                  newPageFunction = newPageFunction.template[templateName]
+                } else {
+                  eval(eventForCopy)
+                  return
+                }
               }
-            }
-            // 待优化可以单独提出来
-            // 取出参数
-            var parameterArr = []
-            var parameterList = eventForCopy.match(/[^\(\)]+(?=\))/g)
-            
-            if (parameterList && parameterList.length > 0) {
-              // 参数列表
-              parameterArr = parameterList[0].split(',')
-              // 进一步处理参数
+              // 待优化可以单独提出来
+              // 取出参数
+              var parameterArr = []
+              var parameterList = eventForCopy.match(/[^\(\)]+(?=\))/g)
               
-              for (var i = 0; i < parameterArr.length; i++) {
-                var parameterValue = parameterArr[i].replace(/(^\s*)|(\s*$)/g, "")
-                // console.log(parameterValue)
-                // 判断参数是否为一个字符串
+              if (parameterList && parameterList.length > 0) {
+                // 参数列表
+                parameterArr = parameterList[0].split(',')
+                // 进一步处理参数
                 
-                if (parameterValue.charAt(0) === '"' && parameterValue.charAt(parameterValue.length - 1) === '"') {
-                  parameterArr[i] = parameterValue.substring(1, parameterValue.length - 1)
+                for (var i = 0; i < parameterArr.length; i++) {
+                  var parameterValue = parameterArr[i].replace(/(^\s*)|(\s*$)/g, "")
+                  // console.log(parameterValue)
+                  // 判断参数是否为一个字符串
+                  
+                  if (parameterValue.charAt(0) === '"' && parameterValue.charAt(parameterValue.length - 1) === '"') {
+                    parameterArr[i] = parameterValue.substring(1, parameterValue.length - 1)
+                  }
+                  if (parameterValue.charAt(0) === "'" && parameterValue.charAt(parameterValue.length - 1) === "'") {
+                    parameterArr[i] = parameterValue.substring(1, parameterValue.length - 1)
+                  }
+                  // console.log(parameterArr[i])
                 }
-                if (parameterValue.charAt(0) === "'" && parameterValue.charAt(parameterValue.length - 1) === "'") {
-                  parameterArr[i] = parameterValue.substring(1, parameterValue.length - 1)
-                }
-                // console.log(parameterArr[i])
+                eventForCopy = eventForCopy.replace('(' + parameterList + ')', '')
+              } else {
+                // 解决 @click="xxx()"会造成的问题
+                eventForCopy = eventForCopy.replace('()', '')
               }
-              eventForCopy = eventForCopy.replace('(' + parameterList + ')', '')
-            } else {
-              // 解决 @click="xxx()"会造成的问题
-              eventForCopy = eventForCopy.replace('()', '')
-            }
-            // console.log(newPageFunction)
-            // 如果有方法,则运行它
-            if (newPageFunction[eventForCopy]) {
-              // 绑定window.owo对象
-              // console.log(tempDom)
-              // 待测试不知道这样合并会不会对其它地方造成影响
-              newPageFunction.$el = this
-              newPageFunction.$event = event
-              newPageFunction[eventForCopy].apply(newPageFunction, parameterArr)
-            } else {
-              // 如果没有此方法则交给浏览器引擎尝试运行
-              eval(eventForCopy)
+              // console.log(newPageFunction)
+              // 如果有方法,则运行它
+              if (newPageFunction[eventForCopy]) {
+                // 绑定window.owo对象
+                // console.log(tempDom)
+                // 待测试不知道这样合并会不会对其它地方造成影响
+                newPageFunction.$el = this
+                newPageFunction.$event = event
+                newPageFunction[eventForCopy].apply(newPageFunction, parameterArr)
+              } else {
+                // 如果没有此方法则交给浏览器引擎尝试运行
+                eval(eventForCopy)
+              }
             }
           }
         }
       }
     }
   }
+  
 
   // 递归处理所有子Dom结点
   for (var i = 0; i < tempDom.children.length; i++) {
