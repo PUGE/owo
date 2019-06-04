bef7309bb6e7059bc64cc05751123254662c166b
PUGE <100284685@qq.com>
2019-06-04 23:55:57 +0800

    save

--- core/main.js
+++ core/main.js
@@ -13,12 +13,16 @@ owo.tool = {}
 // 事件推送方法
 var $emit = function (eventName) {
   var event = owo.state.event[eventName]
+  let argumentsList = []
+  for (let ind = 1; ind < arguments.length; ind++) {
+    argumentsList.push(arguments[ind])
+  }
   event.forEach(element => {
     // 注入运行环境运行
     element.fun.apply(_owo.assign(element.script, {
       $el: element.dom,
       activePage: window.owo.activePage
-    }), arguments)
+    }), argumentsList)
   })
 }
 
