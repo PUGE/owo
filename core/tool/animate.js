87cdd4ab18cf5c76e350308e62f26cf53e33370b
puge <mail@puge.org>
2019-07-16 17:10:38 +0800

    增加owo.tool.animate方法， 更方便地制作动画效果

---
+++ core/tool/animate.js
@@ -0,0 +1,16 @@
+/**
+ * 赋予节点动画效果
+ * @param  {string} name 动画效果名称
+ * @param  {dom} dom 节点
+ */
+owo.tool.animate = function (name, dom) {
+  dom.classList.add(name)
+  dom.classList.add('owo-animated')
+  // 待优化可以单独提出绑定方法
+  dom.addEventListener('animationend', animateEnd)
+  function animateEnd () {
+    // 待优化 感觉不需要这样
+    dom.classList.remove(name)
+    dom.classList.remove('owo-animated')
+  }
+}
