99c7a3a101022408c6b2fce1dd301d13c0077e07
puge <mail@puge.org>
2019-09-05 16:30:01 +0800

    toast支持自定义字体大小

--- core/tool/toast.js
+++ core/tool/toast.js
@@ -1,14 +1,16 @@
 /**
  * 显示toast提示 不支持ie8
  * @param  {number} text       显示的文字
+ * @param  {number} fontSize   字体大小
  * @param  {number} time       显示时长
  */
 
-owo.tool.toast = (text, time) => {
+owo.tool.toast = (text, size, time) => {
   if (window.owo.state.toastClock) {
     clearTimeout(window.owo.state.toastClock)
     hideToast()
   }
+  var fontSize = size || 14
   if (time === undefined || time === null) {
     // 默认2秒
     time = 2000
@@ -17,7 +19,7 @@ owo.tool.toast = (text, time) => {
   toast.setAttribute("id", "toast")
   toast.setAttribute("class", "toast")
   // 设置样式
-  toast.style.cssText = "position:fixed;z-index:999;background-color:rgba(0, 0, 0, 0.5);bottom:10%;line-height:40px;border-radius:10px;left:0;right:0;margin:0 auto;text-align:center;color:white;max-width:200px;padding:0 10px;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;"
+  toast.style.cssText = "position:fixed;z-index:999;background-color:rgba(0, 0, 0, 0.5);bottom:10%;border-radius:10px;left:0;right:0;margin:0 auto;text-align:center;color:white;max-width:330px;padding:15px 10px;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;font-size:" + fontSize + 'px;'
 
   toast.innerHTML = text
   document.body.appendChild(toast)
