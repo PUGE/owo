d053e3dce9c74dc6166dc1b8c32c1bd7f558952c
PUGE <100284685@qq.com>
2019-10-10 22:34:44 +0800

    修改emit方法

--- core/tool/emit.js
+++ core/tool/emit.js
@@ -1,13 +1,27 @@
 
 // 事件推送方法
 owo.tool.emit = function (eventName) {
-  var event = owo.state.event[eventName]
   let argumentsList = []
   for (let ind = 1; ind < arguments.length; ind++) {
     argumentsList.push(arguments[ind])
   }
-  event.forEach(element => {
-    // 注入运行环境运行
-    element.fun.apply(element.script, argumentsList)
-  })
+  for (const key in owo.script) {
+    if (owo.script.hasOwnProperty(key)) {
+      const page = owo.script[key];
+      if (page.broadcast && page.broadcast[eventName]) {
+        page.broadcast[eventName].apply(page, argumentsList)
+      }
+      // 判断是否有组件
+      if (page.template) {
+        for (const key in page.template) {
+          if (page.template.hasOwnProperty(key)) {
+            const template = page.template[key];
+            if (template.broadcast && template.broadcast[eventName]) {
+              template.broadcast[eventName].apply(template, argumentsList)
+            }
+          }
+        }
+      }
+    }
+  }
 }
