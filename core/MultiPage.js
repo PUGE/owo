863f530fff8e8b17a3527181e0aaa56bc9c61975
PUGE <100284685@qq.com>
2019-10-10 22:57:08 +0800

    增加phoneEnter配置，支持手机单独指定入口

--- core/MultiPage.js
+++ core/MultiPage.js
@@ -19,6 +19,10 @@ _owo.showPage = function() {
   end */
   // 从配置项中取出程序入口
   var page = pageArg ? pageArg : owo.entry
+  /* if="phoneEnter"
+  // 手机进入特制页
+  if (_owo.isMobi) {page = owo.phoneEnter}
+  end */
   if (page) {
     var entryDom = document.querySelector('.ox[template="' + page + '"]')
     if (entryDom) {
