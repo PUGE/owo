e951fbbb12f962283a2e98ebe9b2a8a502f2dd13
puge <mail@puge.org>
2020-03-04 23:51:22 +0800

    初步增加handleLink

--- control/dist/static/js/owo.main.p5gzdex1d19.js
+++ control/dist/static/js/owo.main.p5gzdex1d19.js
@@ -1,6 +1,6 @@
 
 console.log('ss')
-// Wed Mar 04 2020 12:44:09 GMT+0800 (GMT+08:00)
+// Wed Mar 04 2020 23:50:24 GMT+0800 (GMT+08:00)
 var owo = {tool: {},state: {},};
 /* 方法合集 */
 var _owo = {}
@@ -888,21 +888,4 @@ owo.tool.post = function (url, data, fn) {
 }
 
 
-
-
-// 这是用于代码调试的自动刷新代码，他不应该出现在正式上线版本!
-if ("WebSocket" in window) {
-  // 打开一个 web socket
-  if (!window._owo.ws) window._owo.ws = new WebSocket("ws://" + window.location.host)
-  window._owo.ws.onmessage = function (evt) { 
-    if (evt.data == 'reload') {
-      location.reload()
-    }
-  }
-  window._owo.ws.onclose = function() { 
-    console.info('与服务器断开连接')
-  }
-} else {
-  console.error('浏览器不支持WebSocket')
-}
 
