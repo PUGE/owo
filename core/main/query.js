9629d6303314830357a435706b31457de0580e06
puge <mail@puge.org>
2020-05-21 00:48:30 +0800

    增加_owo.addHTMLElementFun方法

--- core/main/query.js
+++ core/main/query.js
@@ -2,3 +2,15 @@
 owo.query = function (str) {
   return document.querySelectorAll('.page[template=' + owo.activePage +'] ' + str)
 }
+_owo.addHTMLElementFun = function (name, func) {
+  if (window.HTMLElement) {
+    HTMLElement.prototype[name] = func
+  } else {
+    for (var ind=0; ind < document.all.length; ind++) {
+      document.all[ind][name] = func
+    }
+  }
+}
+_owo.addHTMLElementFun('query', function(str) {
+  return this.querySelector(str)
+})
