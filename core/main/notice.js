ea15ee345904fad94494f1c0358e4b9a7c2abb8f
puge <mail@puge.org>
2021-03-23 16:56:18 +0800

    增加owo.notice方法

---
+++ core/main/notice.js
@@ -0,0 +1,16 @@
+// 向各个组件发送通知，暂时不支持参数
+owo.notice = function (str) {
+  function check (el) {
+    for (const key in el) {
+      if (Object.hasOwnProperty.call(el, key)) {
+        const element = el[key];
+        if (element.notice && element.notice[str]) {
+          element.notice[str].apply(element)
+        }
+        if (element.template) check(element.template)
+        console.log(element)
+      }
+    }
+  }
+  check(owo.script)
+}
