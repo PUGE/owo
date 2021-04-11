a834b81ace5781208cbe80179b62f9071cae85c1
puge <mail@puge.org>
2021-04-11 20:51:27 +0800

    修复通知功能无法通知子路由的问题

--- core/main/notice.js
+++ core/main/notice.js
@@ -1,14 +1,20 @@
 // 向各个组件发送通知，暂时不支持参数
 owo.notice = function (str) {
   function check (el) {
-    for (const key in el) {
+    for (var key in el) {
       if (Object.hasOwnProperty.call(el, key)) {
         const element = el[key];
         if (element.notice && element.notice[str]) {
           element.notice[str].apply(element)
         }
         if (element.template) check(element.template)
-        // console.log(element)
+        if (element.view) {
+          for (var tempKey in element.view) {
+            if (Object.hasOwnProperty.call(element.view, tempKey)) {
+              check(element.view[tempKey])
+            }
+          }
+        }
       }
     }
   }
